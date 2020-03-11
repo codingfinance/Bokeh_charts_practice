27112197943fd04fcbc19f4d6d371bba270cabb8
DD <codingfinance@protonmail.com>
2020-03-10 21:54:14 -0400

    Add rbokeh

---
+++ 01_rbokeh.r
@@ -0,0 +1,29 @@
+library(rbokeh)
+library(tidyverse)
+
+
+# Scatter Plot ------------------------------------------------------------
+
+p <- figure() %>%
+  ly_points(Sepal.Length, Sepal.Width, data = iris,
+            color = Species, glyph = Species,
+            hover = list(Sepal.Length, Sepal.Width))
+p
+
+
+# Scatter with best fit line ----------------------------------------------
+
+z <- lm(dist ~ speed, data = cars)
+p <- figure(width = 600, height = 600) %>%
+  ly_points(cars, hover = cars) %>%
+  ly_lines(lowess(cars), legend = "lowess") %>%
+  ly_abline(z, type = 2, legend = "lm")
+p
+
+
+# Bimodal Distribution ----------------------------------------------------
+
+h <- figure(width = 600, height = 400) %>%
+  ly_hist(eruptions, data = faithful, breaks = 40, freq = FALSE) %>%
+  ly_density(eruptions, data = faithful)
+h
